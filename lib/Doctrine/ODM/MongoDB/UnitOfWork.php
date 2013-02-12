aa4c18fc5a2c594b45adeae82fc19e3fb48da1bc
Jeremy Mikola <jmikola@gmail.com>
2013-02-12 15:46:58 -0500

    Allow for non-null, falsey id values in getDocumentState()

--- lib/Doctrine/ODM/MongoDB/UnitOfWork.php
+++ lib/Doctrine/ODM/MongoDB/UnitOfWork.php
@@ -1614,7 +1614,7 @@ class UnitOfWork implements PropertyChangedListener
          */
         $id = $class->getIdentifierValue($document);
 
-        if ( ! $id) {
+        if ($id === null) {
             return self::STATE_NEW;
         }
 
